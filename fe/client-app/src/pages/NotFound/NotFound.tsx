18cd4fe481bb2009c06b547830509badbf9e0386
Maksym Zaitsev <102425029+maksymzaitsev20@users.noreply.github.com>
2024-03-24 12:20:20 +0200

    [FE] Added min text for the NotFound page

--- fe/client-app/src/pages/NotFound/NotFound.tsx
+++ fe/client-app/src/pages/NotFound/NotFound.tsx
@@ -1,7 +1,9 @@
 const NotFound = () => {
   return (
     <div>
-      <h1>404 Not Found</h1>
+      <h3>Oops, you tried to open a non-existent page...</h3>
+        <p>Sorry, we couldn't find the page you were looking for</p>
+        <a href="/">Go back to the home page</a>
     </div>
   );
 };
